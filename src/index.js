80279706281fc87e8e819cadb001432720e91243
sananWEB <sanankhan16@gmail.com>
2020-06-25 10:01:53 -0700

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -13,3 +13,4 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 
+serviceWorker.unregister();
